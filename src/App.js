67a3f0edb583b349a5ac946329c521071322c279
modrayyes <mod.rayyes@gmail.com>
2021-09-15 17:11:47 +0300

    main structure

---
+++ src/App.js
@@ -0,0 +1,61 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { Component } from 'react';
+
+class App extends Component{
+
+    state={
+      items:[],
+      isLoaded : false,  
+    }
+    componentDidMount(){
+      fetch('https://crossorig.in/https://yts.am/api/v2/list_movies.json')
+          .then(res => res.json())
+          .then(json=>{
+            this.setState({
+              isLoaded : true,
+              items: json,
+            })
+          });
+
+    }
+
+
+  render(){
+    
+
+    
+    var {isLoaded , items} = this.state
+    console.log( 'itemssssssssssssssssssssssssss' ,items)
+    console.log()
+
+    if(!isLoaded){
+      return <div>Loading....</div>;
+    }
+    else{
+          return (
+            <div className="App">
+                <ul>
+                  {items.data.movies.map(movie =>
+                   
+                    <ul key={movie.id} className="card">
+                      <h3>{movie.title}</h3>
+                      <p> IMBD {movie.imdb_code}</p>
+                      <a href={movie.url}> Link</a>
+
+                      <p>{movie.description}</p>
+                      <img src={movie.medium_cover_image} alt="img" />
+                      
+                    </ul>
+                     )};
+                </ul>
+            </div>
+          );
+        }
+    }
+
+  }
+ 
+
+export default App;
